3a91c609ee746162bc2c8db8a7eb83a6ed7f7e1d
arisa2545 <arisa.ice@odds.team>
2023-09-19 23:32:50 +0700

    remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,5 @@
 import { testApi } from "@/services/TestApi"
 import { useEffect, useState } from "react"
-import axios from "axios";
-import { API_URL } from "@/config/constants";
-
-const client = axios.create({
-  baseURL: API_URL
-});
 
 export default function Home() {
   const [user, setUser] = useState<string>()
