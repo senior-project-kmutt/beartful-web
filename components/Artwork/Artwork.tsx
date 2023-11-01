d0032614c1099aacb57c7c21e6ae03cb277a3af3
mottdy <mottdy22813@gmail.com>
2023-11-02 00:04:17 +0700

    artwork detail

--- components/Artwork/Artwork.tsx
+++ components/Artwork/Artwork.tsx
@@ -4,9 +4,12 @@ import { useEffect, useState } from "react";
 import style from "@/styles/artwork/artworkLayout.module.scss";
 import ArtworkItem from "./ArtworkItem";
 import ArtworkCategory from "./ArtworkCategory";
+import ArtworkDetail from "./ArtworkDetail";
 
 const Artwork = () => {
   const [artwork, setArtwork] = useState<Artwork[]>();
+  const [isShowDetail, setIsShowDetail] = useState<boolean>(false);
+
   useEffect(() => {
     getArtwork().subscribe((res) => {
       setArtwork(res.data);
@@ -15,14 +18,17 @@ const Artwork = () => {
   return (
     <div className="flex">
       <ArtworkCategory />
-      <div className={style.artwork_container}>
-        {artwork?.map((item) => {
-          return (
-            <>
-              <ArtworkItem item={item} />
-            </>
-          );
-        })}
+      <div className={style.container}>
+        {true && <ArtworkDetail />}
+        <div className={style.artwork_container}>
+          {artwork?.map((item) => {
+            return (
+              <>
+                <ArtworkItem item={item} />
+              </>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
