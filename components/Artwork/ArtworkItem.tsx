8bd32d06af1de1b8193d103f35e5a9f5fa7e7bf0
mottdy <mottdy22813@gmail.com>
2023-11-01 19:56:17 +0700

    style css

--- components/Artwork/ArtworkItem.tsx
+++ components/Artwork/ArtworkItem.tsx
@@ -1,42 +1,22 @@
 import { Artwork } from "@/models/artwork";
 import ArtworkImage from "./ArtworkImage";
 import { useEffect, useRef, useState } from "react";
+import styled from "@/styles/artwork/artworkLayout.module.scss";
 interface Props {
   item: Artwork;
 }
 const ArtworkItem = (props: Props) => {
   const item = props;
-  const [rowSpan, setRowSpan] = useState<number>();
-  const imgRef = useRef<HTMLImageElement | null>(null);
-
-  const artworkLayout = () => {
-    const img = new Image();
-    img.src = item.item.images[0];
-    if (imgRef.current) {
-      imgRef.current.onload = () => {
-        const imageAspectRatio = img.naturalWidth / img.naturalHeight;
-        const newRowSpan = Math.ceil(
-          (imgRef.current?.offsetWidth ?? 0) / imageAspectRatio / 10
-        );
-        if (newRowSpan !== null) {
-          setRowSpan(newRowSpan);
-        }
-      };
-    }
-  }
-
-  useEffect(() => {
-    artworkLayout()
-  }, []);
 
   const style = {
     src: item.item.images[0],
-    size: `span ${rowSpan}`,
   };
 
   return (
     <>
-      <ArtworkImage ref={imgRef} theme={style} />
+      <div className={styled.artwork_box}>
+        <ArtworkImage theme={style} />
+      </div>
     </>
   );
 };
