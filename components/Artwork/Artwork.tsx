136bea0ee9eeaf547cd2c58ab12a361ed6cbf837
mottdy <mottdy22813@gmail.com>
2023-11-01 22:36:16 +0700

    Add category layout

--- components/Artwork/Artwork.tsx
+++ components/Artwork/Artwork.tsx
@@ -3,6 +3,7 @@ import { getArtwork } from "@/services/artwork/artwork.api";
 import { useEffect, useState } from "react";
 import style from "@/styles/artwork/artworkLayout.module.scss";
 import ArtworkItem from "./ArtworkItem";
+import ArtworkCategory from "./ArtworkCategory";
 
 const Artwork = () => {
   const [artwork, setArtwork] = useState<Artwork[]>();
@@ -12,14 +13,17 @@ const Artwork = () => {
     });
   }, []);
   return (
-    <div className={style.artwork_container}>
-      {artwork?.map((item) => {
-        return (
-          <>
-            <ArtworkItem item={item} />
-          </>
-        );
-      })}
+    <div className="flex">
+      <ArtworkCategory />
+      <div className={style.artwork_container}>
+        {artwork?.map((item) => {
+          return (
+            <>
+              <ArtworkItem item={item} />
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 };
