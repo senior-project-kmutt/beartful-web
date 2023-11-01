6894f6a2f2d309b3136bc0351954df91c7f4f8a1
mottdy <mottdy22813@gmail.com>
2023-11-01 22:36:39 +0700

    Add category layout

---
+++ components/Artwork/ArtworkCategory.tsx
@@ -0,0 +1,11 @@
+import style from "@/styles/artwork/artworkLayout.module.scss";
+const ArtworkCategory = () => {
+  return (
+    <div className={style.artwork_category}>
+      <h1>Hiring</h1>
+      <h1>Ready Made</h1>
+    </div>
+  );
+};
+
+export default ArtworkCategory;
