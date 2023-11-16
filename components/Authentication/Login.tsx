6d345e399f64ed0edb1a58836bd6033b63f903c3
arisa2545 <arisa8533@gmail.com>
2023-11-17 02:55:02 +0700

    login page

--- components/Authentication/Login.tsx
+++ components/Authentication/Login.tsx
@@ -1,11 +1,16 @@
 import { LoginUser } from "@/models/users";
 import { login } from "@/services/user/user.api";
 import bcrypt from "bcryptjs";
+import Link from "next/link";
+import style from "@/styles/login/loginLayout.module.scss"
+import { useState } from "react";
+import Router from 'next/router';
 
 interface formDataType {
   [key: string]: any;
 }
 const Login = () => {
+  const [activeMenu, setActiveMenu] = useState<string>('sign in');
   const submitForm = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const responseBody: formDataType = {};
@@ -16,23 +21,64 @@ const Login = () => {
     }
 
     if (responseBody) {
-      login(responseBody as unknown as LoginUser);
+      const res = login(responseBody as unknown as LoginUser).subscribe((res: any) => {
+        localStorage.setItem("auth", res.data.token);
+        Router.push('/');
+      })
     }
   };
   return (
-    <div>
-      <form onSubmit={submitForm}>
-        <h1>Login</h1>
-        <label>
-          Username:
-          <input name="username" type="text" />
-        </label>
-        <label>
-          Password:
-          <input name="password" type="text" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+    <div className={style.main}>
+      <div>
+        <div className={style.warp}>
+          <div className={style.switch}>
+            <div
+              onClick={() => setActiveMenu('sign in')}
+              className={activeMenu === 'sign in' ? `${style.item_active}` : `${style.item}`}
+            >
+              SIGN IN
+            </div>
+            <div
+              onClick={() => setActiveMenu('sign up')}
+              className={activeMenu === 'sign up' ? `${style.item_active}` : `${style.item}`}
+            >
+              SIGN UP
+            </div>
+          </div>
+        </div>
+
+        {activeMenu === 'sign in' && (
+          <div className={`${style.warp} mt-20`}>
+            <form onSubmit={submitForm}>
+              <div className={style.input_form}>
+                <label>
+                  USERNAME
+                  <input name="username" type="text" />
+                </label>
+              </div>
+              <div className={style.input_form}>
+                <label>
+                  PASSWORD
+                  <input name="password" type="password" />
+                </label>
+              </div>
+              <div className="ml-32">
+                <input className={`${style.submit_button} mr-4`} type="submit" value="SIGN IN" />
+                <Link className={style.cancle_button} href="/">CANCLE</Link>
+                <p className="text-xs pt-3 pl-2 font-medium">New to BeArtFul?&nbsp;
+                  <span onClick={() => setActiveMenu('sign up')} className="underline cursor-pointer">Create an account</span>
+                </p>
+              </div>
+            </form>
+          </div>
+        )}
+
+        {activeMenu === 'sign up' && (
+          <div className={`${style.warp} p-16`}>
+            WAIT FOR REGISTER!
+          </div>
+        )}
+      </div>
     </div>
   );
 };
