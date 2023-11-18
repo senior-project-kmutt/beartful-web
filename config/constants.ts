efcfa1218c7ae67548bad490e8faa85a33362355
arisa2545 <arisa8533@gmail.com>
2023-11-19 02:19:26 +0700

    change socker conect url

--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,4 @@
 export const API_URL = 'http://localhost:8000/api';
+export const SOCKET_CONNECT_URL = "http://localhost:8000"
 
 
--- config/socket.ts
+++ config/socket.ts
@@ -1,3 +1,4 @@
 import { io } from 'socket.io-client';
+import { SOCKET_CONNECT_URL } from './constants';
 
-export const socket = io('http://localhost:3006', { transports : ['websocket'] });
+export const socket = io(SOCKET_CONNECT_URL, { transports : ['websocket'] });
