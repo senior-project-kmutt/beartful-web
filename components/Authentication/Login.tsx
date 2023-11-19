91ce6642ed2b4b0a8cf44e2b15a8af46d1cc9276
arisa2545 <68777796+arisa2545@users.noreply.github.com>
2023-11-20 00:10:48 +0700

    Fix API Login (#4)
    * login page
    
    * add swalfire to show pop-up error
    
    * fix css for responsive
    
    * set user to LocalStorage

--- components/Authentication/Login.tsx
+++ components/Authentication/Login.tsx
@@ -1,11 +1,17 @@
 import { LoginUser } from "@/models/users";
 import { login } from "@/services/user/user.api";
 import bcrypt from "bcryptjs";
+import Link from "next/link";
+import style from "@/styles/login/loginLayout.module.scss"
+import { useState } from "react";
+import Router from 'next/router';
+import Swal from 'sweetalert2'
 
 interface formDataType {
   [key: string]: any;
 }
 const Login = () => {
+  const [activeMenu, setActiveMenu] = useState<string>('sign in');
   const submitForm = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const responseBody: formDataType = {};
@@ -16,23 +22,76 @@ const Login = () => {
     }
 
     if (responseBody) {
-      login(responseBody as unknown as LoginUser);
+      const res = login(responseBody as unknown as LoginUser).subscribe((res: any) => {
+        localStorage.setItem("auth", res.data.token);
+        localStorage.setItem("user", JSON.stringify(res.data.user));
+        Router.push('/');
+      }, error => {
+        console.log(error);
+        if (error.response?.status === 401) {
+          Swal.fire({
+          icon: "error",
+          title: "Please Try Again!",
+          text: " username or password not correct"
+        });
+        } else {
+          console.log(error);
+        }
+      })
     }
   };
   return (
-    <div>
-      <form onSubmit={submitForm}>
-        <h1>Login</h1>
-        <label>
-          Username:
-          <input name="username" type="text" />
-        </label>
-        <label>
-          Password:
-          <input name="password" type="text" />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+    <div className={style.main}>
+      <div>
+        <div className={style.warp}>
+          <div className={style.switch}>
+            <div
+              onClick={() => setActiveMenu('sign in')}
+              className={activeMenu === 'sign in' ? `${style.item_active}` : `${style.item}`}
+            >
+              SIGN IN
+            </div>
+            <div
+              onClick={() => setActiveMenu('sign up')}
+              className={activeMenu === 'sign up' ? `${style.item_active}` : `${style.item}`}
+            >
+              SIGN UP
+            </div>
+          </div>
+        </div>
+
+        {activeMenu === 'sign in' && (
+          <div className={`${style.warp} mt-20`}>
+            <form onSubmit={submitForm}>
+              <div className={style.input_form}>
+                <label>
+                  USERNAME
+                  <input name="username" type="text" />
+                </label>
+              </div>
+              <div className={style.input_form}>
+                <label>
+                  PASSWORD
+                  <input name="password" type="password" />
+                </label>
+              </div>
+              <div className={style.input_button}>
+                <input className={`${style.submit_button} mr-4`} type="submit" value="SIGN IN" />
+                <Link className={style.cancel_button} href="/">CANCEL</Link>
+                <p className="text-xs pt-3 pl-2 font-medium">New to BeArtFul?&nbsp;
+                  <span onClick={() => setActiveMenu('sign up')} className="underline cursor-pointer">Create an account</span>
+                </p>
+              </div>
+            </form>
+          </div>
+        )}
+
+        {activeMenu === 'sign up' && (
+          <div className={`${style.warp} p-16`}>
+            WAIT FOR REGISTER!
+          </div>
+        )}
+      </div>
     </div>
   );
 };
