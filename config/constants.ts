fe7be3ced6bfc88cebaad028ee7724da14ec0ce7
arisa2545 <arisa8533@gmail.com>
2023-12-04 20:34:17 +0700

    chage constant api

--- config/constants.ts
+++ config/constants.ts
@@ -1,4 +1,7 @@
-export const API_URL = 'http://localhost:8000/api';
-export const SOCKET_CONNECT_URL = "http://localhost:8000"
+// export const API_URL = 'http://localhost:8000/api';
+// export const SOCKET_CONNECT_URL = "http://localhost:8000"
+
+export const API_URL = 'http://cp23ssi1.sit.kmutt.ac.th:8000/api';
+export const SOCKET_CONNECT_URL = "http://cp23ssi1.sit.kmutt.ac.th:8000"
 
 
