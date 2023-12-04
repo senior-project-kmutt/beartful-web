b005290a98fcbe67f05224288690aaadd99eca75
arisa2545 <arisa8533@gmail.com>
2023-12-05 01:11:56 +0700

    fix nginx.conf 3

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const nextConfig = {
   basePath: '/ssi1',
+  trailingSlash: true,
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
