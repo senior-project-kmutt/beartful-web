67b9850482f1d9bb7230be631ab7397e5521c7c5
mottdy <mottdy22813@gmail.com>
2023-12-07 21:01:45 +0700

    edit config nginx21

--- config/constants.ts
+++ config/constants.ts
@@ -1,7 +1,7 @@
 // export const API_URL = 'http://localhost:8000/api';
 // export const SOCKET_CONNECT_URL = "http://localhost:8000"
 
-export const API_URL = 'https://capstone23.sit.kmutt.ac.th/ssi1/api';
+export const API_URL = "http://cp23ssi1.sit.kmutt.ac.th:8000/api"
 export const SOCKET_CONNECT_URL = "http://cp23ssi1.sit.kmutt.ac.th:8000"
 export const BUGKET_STORAGE = "beartful-ef55a.appspot.com/o"
 
