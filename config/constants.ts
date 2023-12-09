0baddf4f40b5d356682f1625478dc7c5787b4c11
arisa2545 <arisa8533@gmail.com>
2023-12-09 17:16:35 +0700

    change constant for server

--- config/constants.ts
+++ config/constants.ts
@@ -1,8 +1,8 @@
-export const API_URL = 'http://localhost:8000/api';
-export const SOCKET_CONNECT_URL = "http://localhost:8000"
+// export const API_URL = 'http://localhost:8000/api';
+// export const SOCKET_CONNECT_URL = "http://localhost:8000"
 
-// export const API_URL = "http://cp23ssi1.sit.kmutt.ac.th:8000/api"
-// export const SOCKET_CONNECT_URL = "http://cp23ssi1.sit.kmutt.ac.th:8000"
+export const API_URL = "http://cp23ssi1.sit.kmutt.ac.th:8000/api"
+export const SOCKET_CONNECT_URL = "http://cp23ssi1.sit.kmutt.ac.th:8000"
 export const BUGKET_STORAGE = "beartful-ef55a.appspot.com/o"
 
 
