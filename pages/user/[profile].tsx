76a82fda28504c20cda398a03d97f1f772e1a94d
mottdy <mottdy22813@gmail.com>
2024-01-09 13:38:42 +0700

    add profile page

---
+++ pages/user/[profile].tsx
@@ -0,0 +1,21 @@
+import Artwork from "@/components/Artwork/Artwork";
+import { Router, useRouter } from "next/router";
+import { useEffect, useState } from "react";
+
+const FreelanceProfile: React.FC = () => {
+    const [username, setUsername] = useState<string>("");
+    const router = useRouter();
+    useEffect(() => {
+        setUsername(String(router.query.profile))
+    }, [router.query.profile])
+
+    // const getUsername = () => {
+    //     return String(router.query.profile)
+    // }
+
+    return (
+        <Artwork isSpecificFreelance={true} username={username} />
+    );
+};
+
+export default FreelanceProfile;
