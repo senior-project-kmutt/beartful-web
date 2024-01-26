f30a3ea3ec24e342e4f503fc786fac173c1e6c7f
mottdy <mottdy22813@gmail.com>
2024-01-27 01:26:10 +0700

    freelance from vustomer view

--- components/User/FreelanceArtwork.tsx
+++ components/User/FreelanceArtwork.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import ArtworkList from "../Artwork/ArtworkList";
 import NavBar from "../Layout/NavBar";
 import UserSideBar from "./UserSideBar";
+import style from "@/styles/user/freelanceArtwork.module.scss"
 
 interface Props {
     username: string;
@@ -18,7 +19,22 @@ const FreelanceArtwork = (props: Props) => {
                 <div className="w-4/6">
                     <UserSideBar type={type} setType={setType} />
                 </div>
-                {(type == 'hired' || type == 'readyMade' || type == '') && <ArtworkList from="freelance" type={type} username={props.username} />}
+                <div>
+                    <div className={style.profileDescription}>
+                        <div className={style.profile}>
+                            <img src="../../../../ssi1/picture/user1.gif" />
+                            <div className={style.name_box}>
+                                <p className={style.username}>mottdy</p>
+                                <p className={style.name}>Chananya Sinphichit</p>
+                            </div>
+                            <button className={style.description}>รายละเอียดเพิ่มเติม</button>
+                        </div>
+                    </div>
+                    {(type == 'hired' || type == 'readyMade' || type == '') && <ArtworkList from="freelance" type={type} username={props.username} />}
+                    {(type == 'Package&Price') && <div>Package&Price</div>}
+                    {(type == 'Review') && <div>Review</div>}
+                </div>
+
             </div>
         </>
     );
