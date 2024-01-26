9df5ae1bb6b4712eff5cf8dddb5192ededf65c2d
mottdy <mottdy22813@gmail.com>
2024-01-26 23:40:31 +0700

    shop link

--- components/Artwork/ArtworkItem.tsx
+++ components/Artwork/ArtworkItem.tsx
@@ -2,9 +2,12 @@ import { Artwork } from "@/models/artwork";
 import ArtworkImage, {
   ArtworkImageContainer,
   ArtworkName,
+  IconArrow,
+  ShopLinkButton,
   ShopPrice,
 } from "./ArtworkStyled";
 import styled from "@/styles/artwork/artworkLayout.module.scss";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {
   item: Artwork;
@@ -24,6 +27,10 @@ const ArtworkItem = (props: Props) => {
         <ArtworkImageContainer>
           <ArtworkName>{name}</ArtworkName>
           <ArtworkImage theme={{ src: images[0] }} />
+          <ShopLinkButton>
+            Maexzomeiei
+            <IconArrow icon={faArrowUp} />
+          </ShopLinkButton>
           <ShopPrice>{type == 'hired' ? 'เรทราคา :' : 'ราคา :'} {price}</ShopPrice>
         </ArtworkImageContainer>
       </div>
