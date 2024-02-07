6be20c6a1120bb49ca261b150721980ca1d34d7b
arisa2545 <arisa8533@gmail.com>
2024-02-08 00:45:13 +0700

    edit profile bank account

--- services/user/user.api.tsx
+++ services/user/user.api.tsx
@@ -1,6 +1,7 @@
 import { Observable } from "rxjs";
-import { post } from "../HttpClient";
+import { getPromise, patchPromise, post } from "../HttpClient";
 import { LoginUser, Users } from "@/models/users";
+import { IncomingHttpHeaders } from "http";
 
 export const createUser = (user: Users): Observable<void> => {
   const url = `/user/register`;
@@ -11,4 +12,17 @@ export const createUser = (user: Users): Observable<void> => {
 export const login = (user: LoginUser): Observable<void> => {
   const url = `/user/login`;
   return post(url, user);
+};
+
+export const getUserById = (userId: string, headers: IncomingHttpHeaders): Promise<any> => {
+  const url = `/user/${userId}`;
+  return getPromise(url, headers);
+};
+
+export const getChatRoomByUserId = (userId: string, headers: IncomingHttpHeaders): Promise<any> => {
+  return getPromise(`/user/${userId}/chatRooms`, headers);
+};
+
+export const updateBankAccount = (userId: string, body: any, headers: IncomingHttpHeaders): Promise<any> => {
+  return patchPromise(`/user/${userId}/editBankAccount`, body, headers);
 };
