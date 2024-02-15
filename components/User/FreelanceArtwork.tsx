5f0f1a376ad2dfd115a817aeec90234b3e0b8889
arisa2545 <arisa8533@gmail.com>
2024-02-16 04:02:03 +0700

    fixed layout and getUserFreelance

--- components/User/FreelanceArtwork.tsx
+++ components/User/FreelanceArtwork.tsx
@@ -1,9 +1,11 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ArtworkList from "../Artwork/ArtworkList";
 import NavBar from "../Layout/NavBar";
 import UserSideBar from "./UserSideBar";
 import style from "@/styles/user/freelanceArtwork.module.scss"
+import { FreelanceUsers } from "@/models/users";
+import { getUserByUsername } from "@/services/user/user.api";
 
 interface Props {
     username: string;
@@ -11,27 +13,36 @@ interface Props {
 
 const FreelanceArtwork = (props: Props) => {
     const [type, setType] = useState<string>('hired');
+    const [freelance, setFreelance] = useState<FreelanceUsers>();
+
+    useEffect(() => {
+        getUserByUsername(props.username).subscribe(res => {
+            setFreelance(res.data);
+        })
+    }, [])
+
+    console.log(freelance);
 
     return (
         <>
             <NavBar />
             <div className="flex mt-8">
-                <div className="w-4/6">
+                <div className="" style={{width: "10%"}}>
                     <UserSideBar type={type} setType={setType} />
                 </div>
-                <div>
+                <div style={{width: "90%"}}>
                     <div className={style.profileDescription}>
                         <div className={style.profile}>
-                            <img src="../../../../ssi1/picture/user1.gif" />
+                            <img src={freelance?.profileImage} />
                             <div className={style.name_box}>
-                                <p className={style.username}>mottdy</p>
-                                <p className={style.name}>Chananya Sinphichit</p>
+                                <p className={style.username}>{freelance?.username}</p>
+                                <p className={style.name}>{freelance?.firstname} {freelance?.lastname}</p>
                             </div>
                             <button className={style.description}>รายละเอียดเพิ่มเติม</button>
                         </div>
                     </div>
                     {(type == 'hired' || type == 'readyMade' || type == '') && <ArtworkList from="freelance" type={type} username={props.username} />}
-                    {(type == 'Package&Price') && <div>Package&Price</div>}
+                    {/* {(type == 'Package&Price') && <div>Package&Price</div>} */}
                     {(type == 'Review') && <div>Review</div>}
                 </div>
 
