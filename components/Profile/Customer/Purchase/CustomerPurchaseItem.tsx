8e20098ff89cceeb9017734ad99fc48642fd3c7a
mottdy <mottdy22813@gmail.com>
2024-02-15 14:56:17 +0700

    seperate status customer

--- components/Profile/Customer/Purchase/CustomerPurchaseItem.tsx
+++ components/Profile/Customer/Purchase/CustomerPurchaseItem.tsx
@@ -4,6 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import ReviewModal from "../../Component/ReviewModal";
 import { ICustomerPurchaseOrder } from "@/models/purchaseOrder";
+import { OrderStatusCustomerEnum } from "@/enums/orders";
+import router from "next/router";
 
 interface Props {
     item: ICustomerPurchaseOrder
@@ -20,12 +22,11 @@ const CustomerPurchaseItem = (props: Props) => {
         <div className={style.purchaseItem}>
             <div className={style.profile}>
                 <FontAwesomeIcon icon={faUser} size="sm" style={{ marginTop: "4px" }}></FontAwesomeIcon>
-                <p className={style.username}> shop name</p>
+                <p className={style.username}>{item.freelanceUsername}</p>
                 <div className="ml-8">
-                    <button className={style.saveButton}>Chat</button>
-                    <button className={style.cancelButton}>View profile</button>
+                    {/* <button className={style.saveButton}>Chat</button> */}
+                    <button className={style.cancelButton} onClick={() => router.push(`${process.env.NEXT_PUBLIC_BASEPATH}/user/${item.freelanceUsername}`)}>View profile</button>
                 </div>
-                <div className={style.status}>#####status</div>
             </div>
             {item.order.map((item, index) => {
                 return (
@@ -34,13 +35,27 @@ const CustomerPurchaseItem = (props: Props) => {
                         <div className={style.detail}>
                             <p className={style.artworkName}>Artwork Name</p>
                             <span className={style.packageName}>[  Package Name  ]</span>
-                            <p className={style.price}>{`xxx`} บาท</p>
+                            <p className={style.price}>{item.purchaseOrder.netAmount} บาท</p>
                         </div>
                         <div className={style.confirm}>
+                            <div className={style.status}>{OrderStatusCustomerEnum[item.purchaseOrder.status as keyof typeof OrderStatusCustomerEnum]}</div>
                             <FontAwesomeIcon icon={faClipboardList} style={{ color: '#5A2810' }} size="2xl"></FontAwesomeIcon>
-                            <button className={style.confirmButton}>ฉันได้ส่งมอบงานแล้ว</button>
-                            {isReviewModalOpen && <ReviewModal openReviewModal={openReviewModal} />}
-                            <button className={style.toReviewButton} onClick={openReviewModal}>ให้คะแนน</button>
+                            {item.purchaseOrder.status === 'success' && (
+                                <div>
+                                    <button className={style.disableConfirmButton}>ฉันได้ตรวจสอบและยอมรับงาน</button>
+                                    <button className={style.disabletoReviewButton}>ให้คะแนน</button>
+                                </div>
+                            )}
+                            {item.purchaseOrder.status === 'pending' && (
+                                <div>
+                                    <button className={style.confirmButton}>ฉันได้ตรวจสอบและยอมรับงาน</button>
+                                    {isReviewModalOpen && <ReviewModal openReviewModal={openReviewModal} />}
+                                    <button className={style.toReviewButton} onClick={openReviewModal}>ให้คะแนน</button>
+                                </div>
+                            )}
+                            {item.purchaseOrder.status === 'cancelled' && (
+                                <div><button className={style.disabletoReviewButton}>ยกเลิกแล้ว</button></div>
+                            )}
                         </div>
                     </div>
                 )
--- components/Profile/Freelance/Purchase/FreelancePurchaseItem.tsx
+++ components/Profile/Freelance/Purchase/FreelancePurchaseItem.tsx
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
 import ReviewModal from "../../Component/ReviewModal";
 import { IFreelancePurchaseOrder, IPurchaseOrder } from "@/models/purchaseOrder";
+import { OrderStatusFreelanceEnum } from "@/enums/orders";
 
 interface Props {
     item: IFreelancePurchaseOrder
@@ -19,12 +20,11 @@ const FreelancePurchaseItem = (props: Props) => {
         <div className={style.purchaseItem}>
             <div className={style.profile}>
                 <FontAwesomeIcon icon={faUser} size="sm" style={{ marginTop: "4px" }}></FontAwesomeIcon>
-                <p className={style.username}> shop name</p>
+                <p className={style.username}>{item.customerUsername}</p>
                 <div className="ml-8">
-                    <button className={style.saveButton}>Chat</button>
-                    <button className={style.cancelButton}>View profile</button>
+                    {/* <button className={style.saveButton}>Chat</button> */}
+                    {/* <button className={style.cancelButton}>View profile</button> */}
                 </div>
-                <div className={style.status}>#####status</div>
             </div>
             {item.order.map((item, index) => {
                 return (
@@ -33,13 +33,27 @@ const FreelancePurchaseItem = (props: Props) => {
                         <div className={style.detail}>
                             <p className={style.artworkName}>Artwork Name</p>
                             <span className={style.packageName}>[  Package Name  ]</span>
-                            <p className={style.price}>{`xxx`} บาท</p>
+                            <p className={style.price}>{item.purchaseOrder.netAmount} บาท</p>
                         </div>
                         <div className={style.confirm}>
+                            <div className={style.status}>{OrderStatusFreelanceEnum[item.purchaseOrder.status as keyof typeof OrderStatusFreelanceEnum]}</div>
                             <FontAwesomeIcon icon={faClipboardList} style={{ color: '#5A2810' }} size="2xl"></FontAwesomeIcon>
-                            <button className={style.confirmButton}>ฉันได้ส่งมอบงานแล้ว</button>
-                            {isReviewModalOpen && <ReviewModal openReviewModal={openReviewModal} />}
-                            <button className={style.toReviewButton} onClick={openReviewModal}>ให้คะแนน</button>
+                            {item.purchaseOrder.status === 'success' && (
+                                <div>
+                                    <button className={style.disableConfirmButton}>ฉันได้ตรวจสอบและยอมรับงาน</button>
+                                    <button className={style.disabletoReviewButton}>ให้คะแนน</button>
+                                </div>
+                            )}
+                            {item.purchaseOrder.status === 'pending' && (
+                                <div>
+                                    <button className={style.confirmButton}>ฉันได้ตรวจสอบและยอมรับงาน</button>
+                                    {isReviewModalOpen && <ReviewModal openReviewModal={openReviewModal} />}
+                                    <button className={style.toReviewButton} onClick={openReviewModal}>ให้คะแนน</button>
+                                </div>
+                            )}
+                            {item.purchaseOrder.status === 'cancelled' && (
+                                <div><button className={style.disabletoReviewButton}>ยกเลิกแล้ว</button></div>
+                            )}
                         </div>
                     </div>
                 )
