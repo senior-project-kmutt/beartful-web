32fe3c80fcb15acd5cd4c99304ec688211e316b1
arisa2545 <arisa8533@gmail.com>
2024-03-08 23:36:07 +0700

    revert comfig constant

--- config/constants.ts
+++ config/constants.ts
@@ -1,8 +1,8 @@
-export const API_URL = 'http://localhost:8000/api';
+// export const API_URL = 'http://localhost:8000/api';
 // export const SOCKET_CONNECT_URL = "http://localhost:8000"
 // export const SOCKET_CONNECT_URL = "http://localhost:8001"
 
-// export const API_URL = "https://capstone23.sit.kmutt.ac.th/ssi1/api"
+export const API_URL = "https://capstone23.sit.kmutt.ac.th/ssi1/api"
 export const SOCKET_CONNECT_URL = "https://capstone23.sit.kmutt.ac.th"
 // export const SOCKET_CONNECT_URL = "https://capstone23.sit.kmutt.ac.th/ssi1/"
 export const BUGKET_STORAGE = "beartful-ef55a.appspot.com/o"
