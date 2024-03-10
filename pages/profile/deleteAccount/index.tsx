f218463f9b427bfa37ab5f67952ae6506cf2ef9e
arisa2545 <68777796+arisa2545@users.noreply.github.com>
2024-03-11 00:22:00 +0700

    Create delete account page ✨ (#29)

---
+++ pages/profile/deleteAccount/index.tsx
@@ -0,0 +1,42 @@
+import NavBar from "@/components/Layout/NavBar";
+import DeleteAccountComponent from "@/components/Profile/Component/DeleteAccountComponent";
+import ProfileSelectBarCustomer from "@/components/Profile/Customer/ProfileSelectBar";
+import ProfileSelectBarFreelance from "@/components/Profile/Freelance/ProfileSelectBar";
+import { IUser } from "@/pages/chat";
+import { useEffect, useState } from "react";
+
+const DeleteAccount = () => {
+    const [user, setUser] = useState<IUser>();
+
+    useEffect(() => {
+        const user: IUser = JSON.parse(localStorage.getItem('user') || '');
+        setUser(user);
+    }, []);
+
+    return (
+        <>
+            <NavBar />
+            <div className="flex">
+                {user?.role === 'freelance' && (
+                    <div style={{ width: "18%" }}>
+                        <ProfileSelectBarFreelance activeMenu="deleteAccount" />
+                    </div>
+                )}
+                {user?.role === 'customer' && (
+                    <div style={{ width: "18%" }}>
+                        <ProfileSelectBarCustomer activeMenu="deleteAccount" />
+                    </div>
+                )}
+                <div className={`m-12`} style={{ width: '80%' }}>
+                    {user && (
+                        <>
+                            <DeleteAccountComponent userId={user?.id} />
+                        </>
+                    )}
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default DeleteAccount;
